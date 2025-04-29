42c370b28b7431c4a2e81edd144924ed86b97c10
GelfTech <gelftech.dev@gmail.com>
2025-04-28 21:57:06 -0600

    test5
    test

--- server.cjs
+++ server.cjs
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Sirve archivos estáticos
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// Maneja todas las rutas para React Router
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
